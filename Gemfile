1f5dcc8a2b874e03723aeefdb5f081b8471d89f2
sato akihide <aki@sato.local>
2019-05-02 15:31:41 +0900

    init deploy

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'unicorn' # アプリケーションサーバのunicorn
 gem 'mini_racer', platforms: :ruby # デプロイ時に必要
+gem "bcrypt_pbkdf", ">= 1.0", "< 2.0"
+gem "ed25519", ">= 1.2", "< 2.0"
 
 
 group :development, :test do
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'test_aws'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/akihid/test_aws'
@@ -11,7 +11,7 @@ set :repo_url, 'https://github.com/akihid/test_aws'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/test_aws'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
